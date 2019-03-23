d264027f3ae7edd85a9225de16d49fa62ca366a1
dota228 <48382007+dota228@users.noreply.github.com>
2019-03-23 21:48:52 +0300

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-module.exports.run = async (bot,message,args) => {
-    message.channel.send('Контента пока нету,сервер в разработке:( ');
-};
-module.exports.help = {
-    name: "content"
-};
