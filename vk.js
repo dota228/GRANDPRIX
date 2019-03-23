1acaeb39d6fb434cce362d6e3de32c0370ca9fd5
dota228 <48382007+dota228@users.noreply.github.com>
2019-03-23 21:49:54 +0300

    Delete vk.js

--- vk.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-module.exports.run = async (bot,message,args) => {
-    message.channel.send('Вот наша группа вк: https://vk.com/grandprixgmod');
-};
-module.exports.help = {
-    name: "vk"
-};
